8eab44019484702caa41f06deee416203b091d5f
Alexis RABILLER <alexis.rabiller@edu.itescia.fr>
2020-11-09 14:59:08 +0100

    caractériste

---
+++ src/com/company/Predator.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Predator {
+    int ageNow ;
+    int ageMax ;
+    static final int ageMaxAll = 40 ;
+
+    int nbLifePointsNow;
+    int nblifePointsNow;
+    static final int nblifePointsAll = 20;
+
+    Smelling smell ;
+    Vision vision ;
+
+    public void hunt(){
+
+    }
+    public void move(){
+
+    }
+    public void mate(){
+
+    }
+    public void eat(){
+
+    }
+    public void drink(){
+
+    }
+}
---
+++ src/com/company/Prey.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Prey {
+    int ageNow ;
+    int ageMax ;
+    static final int ageMaxAll = 30 ;
+
+    int nbLifePointsNow;
+    int nblifePointsNow;
+    static final int nblifePointsAll = 70 ;
+
+    Smelling smell ;
+    Vision vision ;
+
+    public void hunt(){
+
+    }
+    public void move(){
+
+    }
+    public void mate(){
+
+    }
+    public void eat(){
+
+    }
+    public void drink(){
+
+    }
+}
